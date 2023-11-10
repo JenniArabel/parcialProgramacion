df98706c0b131dc9c0c888ad5cfb3363ad484a0c
Sofiaravena <sofi.aravena@gmail.com>
2023-11-10 11:10:41 -0300

    add AgenciaViaje.java add ErrorVenta.java

---
+++ AgenciaViaje.java
@@ -0,0 +1,63 @@
+public class AgenciaViaje {
+    private String domicilio;
+    private String nombre;
+    private int numerotelefonoagencia;
+    //private Cliente cliente;
+    //private PaqueteViaje paqueteviaje;
+
+    public String getdomicilio() {
+        return domicilio;
+    }
+    public void setdomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getnombre() {
+        return nombre;
+    }
+    public void setnombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getnumerotelefonoagencia() {
+        return numerotelefonoagencia;
+    }
+    public void setnumerotelefonoagencia(int numerotelefonoagencia) {
+        this.numerotelefonoagencia = numerotelefonoagencia;
+    }
+
+    //public Cliente getCliente() {
+    //    return cliente;
+    //}
+    //public void setCliente(Cliente cliente) {
+    //    this.cliente = cliente;
+    //}
+
+    //public PaqueteViaje getPaqueteviaje() {
+    //    return paqueteviaje;
+    //}
+
+    //public void setPaqueteviaje(PaqueteViaje paqueteviaje) {
+    //    this.paqueteviaje = paqueteviaje;
+    //}
+
+    public void crearCliente(String nombreCliente) {
+        System.out.println("Nombre del cliente: "+ nombreCliente);
+    }
+
+    public void crearviaje(boolean isCrear){
+        System.out.println("¿Desea crear paquete de viaje? "+ isCrear);
+    }
+
+    public void realizarVenta(boolean isRealizarVentas){
+        System.out.println("¿Desea realizar la venta? "+ isRealizarVentas);
+    }
+
+    // public void composicionCliente(Cliente cliente) {
+    //    this.cliente=cliente;
+    //}
+
+    //public void composicionPaqueteViaje(PaqueteViaje paqueteviaje) {
+    //    this.paqueteviaje=paqueteviaje;
+    //}
+}
---
+++ ErrorVenta.java
@@ -0,0 +1,24 @@
+public class ErrorVenta {
+    private boolean sobreventa;
+
+    public boolean isSobreventa() {
+        return sobreventa;
+    }
+    public void setSobreventa(boolean sobreventa) {
+        this.sobreventa = sobreventa;
+    }
+
+    public ErrorVenta(boolean sobreventa) {
+        this.sobreventa = sobreventa;
+    }
+
+    public void Error(boolean isError){
+        System.out.println("¿Usted tiene una sobreventa? "+isError);
+    }
+    public void devolverdinero(boolean isdevolverdinero){
+        System.out.println("¿Desea un rembolso? "+isdevolverdinero);
+    }
+    public void reprogramar(boolean isreprogramar){
+        System.out.println("¿Desea reprogramar su viaje? "+isreprogramar);
+    }
+}
